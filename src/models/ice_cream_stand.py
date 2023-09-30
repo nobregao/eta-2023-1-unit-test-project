bbd14a2e8decb3a1170d24425afb180a910ca58c
renatão <renataonobrega@MacBook-Pro-de-renatao.local>
2023-09-30 17:27:17 -0300

    refactory ice_cream_stand class; add unit tests '

--- src/models/ice_cream_stand.py
+++ src/models/ice_cream_stand.py
@@ -12,32 +12,66 @@ class IceCreamStand(Restaurant):
         super().__init__(restaurant_name, cuisine_type)
         self.flavors = flavors_list
 
+    # REFACTORY: criada função para formatação da lista de sabores
+    def flavors_formated_list(self):
+        flavor_concatenado = ""
+        for index, flavor in enumerate(self.flavors):
+            if index == len(self.flavors) - 1:
+                flavor_concatenado += f" - {flavor}"
+            else:
+                flavor_concatenado += f" - {flavor},\n"
+
+        return flavor_concatenado
+
     def flavors_available(self):
-        """Percorra a lista de sabores disponíveis e imprima."""
+        """
+        Percorra a lista de sabores disponíveis e retorne.
+        """
         if self.flavors:
-            print("\nNo momento temos os seguintes sabores de sorvete disponíveis:")
-            for flavor in self.flavors:
-                print(f"\t-{flavor}")
-        else:
-            print("Estamos sem estoque atualmente!")
+            # REFACTORY: alterado local de formatação da lista para uma função
+            flavor_concatenado = self.flavors_formated_list()
+
+            # REFACTORY: removido print para adicionar retorno a função
+            return f"No momento temos os seguintes sabores de sorvete disponíveis: \n{flavor_concatenado}"
+
+        # REFACTORY: retirado "else" porque é a última instrução a ser executada
+        # REFACTORY: removido print para adicionar retorno a função
+        return "Estamos sem estoque atualmente!"
 
     def find_flavor(self, flavor):
         """Verifica se o sabor informado está disponível."""
-        if self.flavors:
-            if flavor in self.flavors:
-                print(f"Temos no momento {self.flavors}!")
-            else:
-                print(f"Não temos no momento {self.flavors}!")
+        SEM_ESTOQUE = 0
+
+        # REFACTORY: retirado último "else" porque é a última instrução a ser executada
+        # REFACTORY: removido print para adicionar retorno a função
+        # REFACTORY: alterado lugar do retorno que seria do último else para garantir retorno quando não houver estoque (fail first)
+        if len(self.flavors) == SEM_ESTOQUE:
+            return "Estamos sem estoque atualmente!"
+
+        if flavor in self.flavors:
+            # BUG: está retornando todos os sabores. deveria retornar o flavor do parâmetro
+            # REFACTORY: removido print para adicionar retorno a função
+            return f"Temos no momento {flavor}!"
         else:
-            print("Estamos sem estoque atualmente!")
+            # BUG: está retornando todos os sabores. deveria retornar o flavor do parâmetro
+            # REFACTORY: removido print para adicionar retorno a função
+            return f"Não temos no momento {flavor}!"
 
     def add_flavor(self, flavor):
         """Add o sabor informado ao estoque."""
-        if self.flavors:
-            if flavor in self.flavors:
-                print("\nSabor já disponivel!")
-            else:
-                self.flavors.append(flavor)
-                print(f"{flavor} adicionado ao estoque!")
+        # REFACTORY: removida validação inicial de estoque para ser posssível adicionar,
+        # caso não tenha nenhum estoque de sabores
+        CAMPO_VAZIO = 0
+
+        # MELHORIA: validação para caso sabor adicionado seja inválido
+        if len(flavor) == CAMPO_VAZIO:
+            return "Sabor inválido!"
+
+        if flavor in self.flavors:
+            # REFACTORY: removido print para adicionar retorno a função
+            return f"Sabor já disponível!"
         else:
-            print("Estamos sem estoque atualmente!")
+            self.flavors.append(flavor)
+            # REFACTORY: removido print para adicionar retorno a função
+            return f"{flavor} adicionado ao estoque!"
+
--- test/models/test_ice_cream_stand.py
+++ test/models/test_ice_cream_stand.py
@@ -1,10 +1,73 @@
+from src.models.ice_cream_stand import IceCreamStand
+
+
 class TestIceCreamStand:
+    restaurant_name = "Gelatos do Nordeste"
+    cuisine_type = "sorvete de sabores nordestinos"
+    flavors_list = [
+        "Açaí",
+        "Manga Rosa",
+        "Pitanga",
+        "Goiaba Vermelha",
+        "Seriguela",
+        "Rapadura",
+        "Tapioca",
+        "Bolo de Rolo",
+        "Pamonha",
+        "Beiju de Tapioca"
+    ]
 
-    def test_flavors_available(self):
-        assert False
+    def test_flavors_available_com_estoque(self):
+        ice_cream_stand = IceCreamStand(self.restaurant_name, self.cuisine_type, self.flavors_list)
 
-    def test_find_flavor(self):
-        assert False
+        expected_result = \
+            f"""No momento temos os seguintes sabores de sorvete disponíveis: 
+ - Açaí,
+ - Manga Rosa,
+ - Pitanga,
+ - Goiaba Vermelha,
+ - Seriguela,
+ - Rapadura,
+ - Tapioca,
+ - Bolo de Rolo,
+ - Pamonha,
+ - Beiju de Tapioca"""
 
-    def test_add_flavor(self):
-        assert False
+        assert ice_cream_stand.flavors_available() == expected_result
+
+    def test_flavors_available_sem_estoque(self):
+        ice_cream_stand = IceCreamStand(self.restaurant_name, self.cuisine_type, [])
+
+        expected_result = "Estamos sem estoque atualmente!"
+
+        assert ice_cream_stand.flavors_available() == expected_result
+
+    def test_find_flavor_com_estoque_tem_sabor(self):
+        ice_cream_stand = IceCreamStand(self.restaurant_name, self.cuisine_type, self.flavors_list)
+
+        assert ice_cream_stand.find_flavor("Manga Rosa") == "Temos no momento Manga Rosa!"
+
+    def test_find_flavor_com_estoque_nao_tem_sabor(self):
+        ice_cream_stand = IceCreamStand(self.restaurant_name, self.cuisine_type, self.flavors_list)
+
+        assert ice_cream_stand.find_flavor("Milho Verde") == "Não temos no momento Milho Verde!"
+
+    def test_find_flavor_sem_estoque(self):
+        ice_cream_stand = IceCreamStand(self.restaurant_name, self.cuisine_type, [])
+
+        assert ice_cream_stand.find_flavor("Pamonha") == "Estamos sem estoque atualmente!"
+
+    def test_add_flavor_invalido(self):
+        ice_cream_stand = IceCreamStand(self.restaurant_name, self.cuisine_type, [])
+
+        assert ice_cream_stand.add_flavor("") == "Sabor inválido!"
+
+    def test_add_flavor_valido(self):
+        ice_cream_stand = IceCreamStand(self.restaurant_name, self.cuisine_type, [])
+
+        assert ice_cream_stand.add_flavor("Pamonha") == "Pamonha adicionado ao estoque!"
+
+    def test_add_flavor_ja_existe(self):
+        ice_cream_stand = IceCreamStand(self.restaurant_name, self.cuisine_type, self.flavors_list)
+
+        assert ice_cream_stand.add_flavor("Manga Rosa") == "Sabor já disponível!"
